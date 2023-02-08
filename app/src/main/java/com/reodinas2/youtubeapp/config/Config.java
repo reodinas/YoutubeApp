f1cae0a2808638d73259d46791c15fb157c984d1
reodinas <120348461+reodinas@users.noreply.github.com>
2023-02-08 14:16:28 +0900

    키제거

--- app/src/main/java/com/reodinas2/youtubeapp/config/Config.java
+++ app/src/main/java/com/reodinas2/youtubeapp/config/Config.java
@@ -4,5 +4,5 @@ public class Config {
 
     public static final String BASE_URL = "https://www.googleapis.com";
     public static final String PATH = "/youtube/v3/search";
-    public static final String API_KEY = "AIzaSyAa_LSwoxYAOjdgky5wwfbIRxulYdL6pGs";
+    public static final String API_KEY = "자신의 키를 입력하세요";
 }
